9e5d8b38840b28d58f902ec33c5a5d92512f6ac6
Joshua Roys <roysjosh@gmail.com>
2013-03-08 18:11:41 -0500

    Add license

--- rtlfmac.c
+++ rtlfmac.c
@@ -1,3 +1,25 @@
+/******************************************************************************
+ *
+ * Portions Copyright(c) 2013 Joshua Roys
+ * Portions Copyright(c) 2009-2012  Realtek Corporation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
+ *
+ * The full GNU General Public License is included in this distribution in the
+ * file called LICENSE.
+ *
+ *****************************************************************************/
 
 #include <linux/ieee80211.h>
 #include <linux/module.h>
