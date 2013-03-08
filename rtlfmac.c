62e5d4a2d258ced316085e901d0a8c94245bd5c0
Joshua Roys <roysjosh@gmail.com>
2013-03-08 18:02:09 -0500

    Add chip init up to the firmware download

--- rtlfmac.c
+++ rtlfmac.c
@@ -31,6 +31,30 @@ static int _usbctrl_vendorreq_sync_read(struct usb_device *udev, u8 request,
 	return status;
 }
 
+static int _usbctrl_vendorreq_sync_write(struct usb_device *udev, u8 request,
+					u16 value, u16 index, void *pdata,
+					u16 len)
+{
+	unsigned int pipe;
+	int status;
+	u8 reqtype;
+	int vendorreq_times = 0;
+	static int count;
+
+	pipe = usb_sndctrlpipe(udev, 0); /* write_out */
+	reqtype = REALTEK_USB_VENQT_WRITE;
+
+	do {
+		status = usb_control_msg(udev, pipe, request, reqtype, value,
+					 index, pdata, len, 0); /*max. timeout*/
+	} while (status < 0 && ++vendorreq_times < MAX_USBCTRL_VENDORREQ_TIMES);
+
+	if (status < 0 && count++ < 4)
+		pr_err("reg 0x%x, usbctrl_vendorreq TimeOut! status:0x%x value=0x%x\n",
+		       value, status, le32_to_cpu(*(u32 *)pdata));
+	return status;
+}
+
 static u32 _usb_read_sync(struct usb_device *udev, u32 addr, u16 len)
 {
 	u8 request;
@@ -52,33 +76,70 @@ static u32 _usb_read_sync(struct usb_device *udev, u32 addr, u16 len)
 	return ret;
 }
 
+static int _usb_write_sync(struct usb_device *udev, u32 addr, u32 val, u16 len)
+{
+	u8 request;
+	u16 wvalue;
+	u16 index;
+	u32 *data;
+	int ret;
+
+	data = kmalloc(sizeof(u32), GFP_KERNEL);
+	if (!data)
+		return -ENOMEM;
+	request = REALTEK_USB_VENQT_CMD_REQ;
+	index = REALTEK_USB_VENQT_CMD_IDX;
+
+	wvalue = (u16)addr;
+	*data = val;
+	ret = _usbctrl_vendorreq_sync_write(udev, request, wvalue, index, data, len);
+	kfree(data);
+	return ret;
+}
+
 static u8 rtlfmac_read_byte(struct rtlfmac_cfg80211_priv *priv, u32 addr)
 {
-	//struct device *dev = priv->dev;
-
-	//return (u8)_usb_read_sync(to_usb_device(dev), addr, 1);
 	return _usb_read_sync(priv->usbdev, addr, 1);
 }
 
 static u16 rtlfmac_read_word(struct rtlfmac_cfg80211_priv *priv, u32 addr)
 {
-	//struct device *dev = priv->dev;
-
-	//return (u16)_usb_read_sync(to_usb_device(dev), addr, 2);
 	return _usb_read_sync(priv->usbdev, addr, 2);
 }
 
 static u32 rtlfmac_read_dword(struct rtlfmac_cfg80211_priv *priv, u32 addr)
 {
-	//struct device *dev = priv->dev;
-
-	//return _usb_read_sync(to_usb_device(dev), addr, 4);
 	return _usb_read_sync(priv->usbdev, addr, 4);
+}
+
+static int rtlfmac_write_byte(struct rtlfmac_cfg80211_priv *priv, u32 addr, u8 val)
+{
+	return _usb_write_sync(priv->usbdev, addr, (u32)val, 1);
+}
+
+static int rtlfmac_write_word(struct rtlfmac_cfg80211_priv *priv, u32 addr, u16 val)
+{
+	val = cpu_to_le16(val);
+	return _usb_write_sync(priv->usbdev, addr, (u32)val, 2);
+}
+
+static int rtlfmac_write_dword(struct rtlfmac_cfg80211_priv *priv, u32 addr, u32 val)
+{
+	val = cpu_to_le32(val);
+	return _usb_write_sync(priv->usbdev, addr, val, 4);
 }
 
 /* rtlfmac functions */
 int rtlfmac_fw_cmd(struct rtlfmac_cfg80211_priv *priv, uint8_t code, void *buf, int len)
 {
+#if 0
+	struct sk_buff *skb;
+
+	skb = dev_alloc_skb(len);
+	if (!skb)
+		return -ENOMEM;
+#endif
+
 	return 0;
 }
 
@@ -177,12 +238,174 @@ static const u32 rtlfmac_cipher_suites[] = {
 	WLAN_CIPHER_SUITE_AES_CMAC,
 };
 
-/* cfg80211 functions */
-static struct rtlfmac_cfg80211_priv *rtlfmac_alloc_wiphy()
+/* chip init functions */
+int rtlfmac_chip_init(struct rtlfmac_cfg80211_priv *priv)
+{
+	int loop;
+	u8 val8;
+	u16 val16;
+
+	/* Switch to HW control */
+	val16 = rtlfmac_read_word(priv, REG_SYS_CLKR);
+	if (val16 & FWHW_SEL) {
+		val16 &= ~(FWHW_SEL | SWHW_SEL);
+		rtlfmac_write_word(priv, REG_SYS_CLKR, val16);
+	}
+
+	/* Reset CPU Core, Digital Core and MAC I/O */
+	val16 = rtlfmac_read_word(priv, REG_SYS_FUNC_EN);
+	val16 &= ~(FEN_MREGEN | FEN_DCORE | FEN_CPUEN);
+	rtlfmac_write_word(priv, REG_SYS_FUNC_EN, val16);
+	msleep(20);
+
+	rtlfmac_write_byte(priv, REG_SPS0_CTRL + 1, 0x53);
+	rtlfmac_write_byte(priv, REG_SPS0_CTRL + 0, 0x57);
+
+	/* Enable AFE Macro Block's Bandgap & Mbias */
+	val8 = rtlfmac_read_byte(priv, REG_AFE_MISC);
+	val8 |= AFE_MISC_BGEN;
+	rtlfmac_write_byte(priv, REG_AFE_MISC, val8);
+	val8 |= (AFE_MISC_MBEN | AFE_MISC_I32_EN);
+	rtlfmac_write_byte(priv, REG_AFE_MISC, val8);
+
+	/* Enable PLL Power (LDOA15V) */
+	val8 = rtlfmac_read_byte(priv, REG_LDOA15_CTRL);
+	rtlfmac_write_byte(priv, REG_LDOA15_CTRL, val8 | LDA15_EN);
+
+	/* Enable LDOV12D block */
+	val8 = rtlfmac_read_byte(priv, REG_LDOV12D_CTRL);
+	rtlfmac_write_byte(priv, REG_LDOV12D_CTRL, val8 | LDV12_EN);
+
+	/* Set Digital Vdd to retention isolation path */
+	val16 = rtlfmac_read_word(priv, REG_SYS_ISO_CTRL);
+	rtlfmac_write_word(priv, REG_SYS_ISO_CTRL, val16 | ISO_PWC_DV2RP);
+
+	/* Warm reboot issue / Engineer Packet CP Test Enable */
+	val16 = rtlfmac_read_word(priv, REG_SYS_FUNC_EN);
+	rtlfmac_write_word(priv, REG_SYS_FUNC_EN, val16 | PWC_DV2LDR);
+
+	/* Support 64k IMEM */
+	val8 = rtlfmac_read_byte(priv, REG_SYS_ISO_CTRL + 1);
+	rtlfmac_write_byte(priv, REG_SYS_ISO_CTRL + 1, val8 & ~0x97);
+
+	/* Enable AFE clock */
+	val16 = rtlfmac_read_word(priv, REG_AFE_XTAL_CTRL);
+	rtlfmac_write_word(priv, REG_AFE_XTAL_CTRL, val16 & ~XTAL_GATE_AFE);
+
+	/* Enable AFE PLL Macro Block */
+	val8 = rtlfmac_read_byte(priv, REG_AFE_PLL_CTRL);
+	rtlfmac_write_byte(priv, REG_AFE_PLL_CTRL, val8 | 0x10 | APLL_EN);
+	udelay(500); /* Allow clock to stabilize */
+	rtlfmac_write_byte(priv, REG_AFE_PLL_CTRL, val8 | 0x50 | APLL_EN);
+	udelay(500);
+	rtlfmac_write_byte(priv, REG_AFE_PLL_CTRL, val8 | 0x10 | APLL_EN);
+	udelay(500);
+
+	/* Attach AFE PLL to MACTOP/BB/... */
+	val8 = rtlfmac_read_byte(priv, REG_SYS_ISO_CTRL);
+	rtlfmac_write_byte(priv, REG_SYS_ISO_CTRL, val8 & ~(ISO_PLL2MD | ISO_MD2PP));
+
+	/* Switch to 40MHz clock */
+	rtlfmac_write_byte(priv, REG_SYS_CLKR, 0x00);
+
+	/* Disable CPU clock and 80MHz clock SSC */
+	val8 = rtlfmac_read_byte(priv, REG_SYS_CLKR);
+	rtlfmac_write_byte(priv, REG_SYS_CLKR, val8 | 0xa0);
+
+	/* Enable MAC clock */
+	val16 = rtlfmac_read_word(priv, REG_SYS_CLKR);
+	rtlfmac_write_word(priv, REG_SYS_CLKR, val16 | SYS_SYS_CLK_EN | SYS_MAC_CLK_EN);
+
+	rtlfmac_write_byte(priv, REG_PMC_FSM, 0x02);
+
+	/* Enable Digital Core */
+	val16 = rtlfmac_read_word(priv, REG_SYS_FUNC_EN);
+	rtlfmac_write_word(priv, REG_SYS_FUNC_EN, val16 | FEN_DCORE);
+
+	/* Enable IOREG R/W (MAC I/O) */
+	val16 = rtlfmac_read_word(priv, REG_SYS_FUNC_EN);
+	rtlfmac_write_word(priv, REG_SYS_FUNC_EN, val16 | FEN_MREGEN);
+
+	/* Switch the control path to FW */
+	val16 = rtlfmac_read_word(priv, REG_SYS_CLKR);
+	rtlfmac_write_word(priv, REG_SYS_CLKR, (val16 | FWHW_SEL) & ~SWHW_SEL);
+
+	rtlfmac_write_word(priv, REG_CR, BBRSTn | BB_GLB_RSTn | SCHEDULE_EN |
+			MACRXEN | MACTXEN | DDMA_EN | FW2HW_EN |
+			RXDMA_EN | TXDMA_EN | HCI_RXDMA_EN | HCI_TXDMA_EN);
+
+	/* Fix USB RX FIFO error */
+	val8 = rtlfmac_read_byte(priv, REG_USB_AGG_TO);
+	rtlfmac_write_byte(priv, REG_USB_AGG_TO, val8 | BIT(7));
+
+	/* Save power */
+	val8 = rtlfmac_read_byte(priv, REG_SYS_CLKR);
+	rtlfmac_write_byte(priv, REG_SYS_CLKR, val8 & ~SYS_CPU_CLKSEL);
+
+	/* Prevent incorrect operation of 8051 ROM code */
+	rtlfmac_write_byte(priv, 0xFE1C, 0x80);
+
+	/* Ensure TxDMA is ready for firmware download */
+	for(loop = 0; loop < 20; loop++) {
+		val8 = rtlfmac_read_byte(priv, REG_TCR);
+		if ((val8 & TXDMA_INIT_VALUE) == TXDMA_INIT_VALUE)
+			break;
+		udelay(5);
+	}
+	if (loop >= 20) {
+		/* Reset TxDMA */
+		val8 = rtlfmac_read_byte(priv, REG_CR);
+		rtlfmac_write_byte(priv, REG_CR, val8 & ~TXDMA_EN);
+		udelay(2);
+		rtlfmac_write_byte(priv, REG_CR, val8 | TXDMA_EN);
+	}
+
+	return 0;
+}
+
+/* firmware loading functions */
+static const char firmware_file[] = "rtlwifi/rtl8712u.bin";
+
+static void rtlfmac_fw_cb(const struct firmware *firmware, void *context)
+{
+	struct rtlfmac_cfg80211_priv *priv = context;
+
+	complete(&priv->fw_ready);
+	if (!firmware) {
+		pr_err("%s: firmware request failed\n", __func__);
+		return;
+	}
+
+	priv->fw = firmware;
+
+	rtlfmac_chip_init(priv);
+}
+
+int rtlfmac_load_fw(struct rtlfmac_cfg80211_priv *priv)
+{
+	int rc;
+
+	init_completion(&priv->fw_ready);
+	priv->fw = NULL;
+
+	pr_info("%s: loading firmware from \"%s\"\n", __func__, firmware_file);
+	rc = request_firmware_nowait(THIS_MODULE, 1, firmware_file, priv->dev,
+			GFP_KERNEL, priv, rtlfmac_fw_cb);
+	if (rc) {
+		pr_err("%s: failed to request firmware\n", __func__);
+	}
+
+	return rc;
+}
+
+/* probe helper functions */
+static struct rtlfmac_cfg80211_priv *rtlfmac_alloc_wiphy(void)
 {
 	struct rtlfmac_cfg80211_priv *priv;
 	struct wiphy *wiphy;
+#if 0
 	s32 err = 0;
+#endif
 
 	wiphy = wiphy_new(&rtlfmac_cfg80211_ops,
 			sizeof(struct rtlfmac_cfg80211_priv));
@@ -210,19 +433,23 @@ static struct rtlfmac_cfg80211_priv *rtlfmac_alloc_wiphy()
 	wiphy->signal_type = CFG80211_SIGNAL_TYPE_MBM;
 	//wiphy->flags = ;
 
+#if 0
 	err = wiphy_register(wiphy);
 	if (err < 0) {
 		pr_err("%s: failed wiphy_register: %i\n", __func__, err);
 		wiphy_free(wiphy);
 		return NULL;
 	}
+#endif
 
 	return priv;
 }
 
+/* driver init/de-init functions */
 int rtlfmac_probe(struct usb_interface *intf,
 		const struct usb_device_id *id)
 {
+	int i;
 	struct rtlfmac_cfg80211_priv *priv;
 	struct usb_device *usb = interface_to_usbdev(intf);
 	u8 tmpb;
@@ -249,8 +476,31 @@ int rtlfmac_probe(struct usb_interface *intf,
 			tmpb = 0x2; /* default to BCUT */
 	}
 	priv->hwrev = tmpb;
+	pr_info("%s: hwrev %u\n", __func__, priv->hwrev);
 
-	pr_info("%s: hw rev: %u, leaving\n", __func__, priv->hwrev);
+	/* read eeprom info */
+	tmpb = rtlfmac_read_byte(priv, REG_EPROM_CMD);
+	pr_info("%s: Boot from %s: Autoload %s\n",
+			__func__,
+			(tmpb & EPROM_SEL) ? "EEPROM" : "EFUSE",
+			(tmpb & EPROM_EN) ? "OK" : "Failed");
+	if (tmpb & EPROM_EN) {
+		/* Prevent EFUSE leakage by turning on 2.5V */
+		tmpb = rtlfmac_read_byte(priv, REG_EFUSE_TEST + 3);
+		rtlfmac_write_byte(priv, REG_EFUSE_TEST + 3, tmpb | 0x80);
+		msleep(20);
+		rtlfmac_write_byte(priv, REG_EFUSE_TEST + 3, tmpb & ~0x80);
+	}
+
+	/* read MAC address from registers */
+	for(i = 0; i < ETH_ALEN; i++) {
+		priv->wiphy->perm_addr[i] = rtlfmac_read_byte(priv, REG_USB_MAC_ADDR + i);
+	}
+	pr_info("%s: MAC address from registers: %pM\n", __func__, priv->wiphy->perm_addr);
+
+	rtlfmac_load_fw(priv);
+
+	pr_info("%s: leaving\n", __func__);
 
 	return 0;
 }
@@ -263,7 +513,13 @@ static void rtlfmac_disconnect(struct usb_interface *intf)
 	pr_info("%s: enter\n", __func__);
 
 	priv = dev_get_drvdata(&usb->dev);
+
+	wait_for_completion(&priv->fw_ready);
+	if (priv->fw)
+		release_firmware(priv->fw);
+#if 0
 	wiphy_unregister(priv->wiphy);
+#endif
 	wiphy_free(priv->wiphy);
 }
 
--- rtlfmac.h
+++ rtlfmac.h
@@ -2,6 +2,9 @@
 #ifndef RTLFMAC_H
 #define RTLFMAC_H
 
+#include <linux/completion.h>
+#include <linux/firmware.h>
+
 #define REALTEK_USB_VENQT_READ		0xC0
 #define REALTEK_USB_VENQT_WRITE		0x40
 #define REALTEK_USB_VENQT_CMD_REQ	0x05
@@ -78,6 +81,9 @@ struct rtlfmac_cfg80211_priv {
 	struct wiphy *wiphy;
 
 	u8 hwrev;
+
+	struct completion fw_ready;
+	const struct firmware *fw;
 };
 
 static inline struct rtlfmac_cfg80211_priv *wiphy_to_cfg(struct wiphy *w)
@@ -87,7 +93,68 @@ static inline struct rtlfmac_cfg80211_priv *wiphy_to_cfg(struct wiphy *w)
 
 /* System Configuration Registers */
 #define REG_SYS_ISO_CTRL			0x0000
+#define   ISO_MD2PP				BIT(0)
+#define   ISO_PLL2MD				BIT(4)
+#define   ISO_PWC_DV2RP				BIT(11)
 #define REG_SYS_FUNC_EN				0x0002
+#define   FEN_CPUEN				BIT(10)
+#define   FEN_DCORE				BIT(11)
+#define   PWC_DV2LDR				BIT(13)
+#define   FEN_MREGEN				BIT(15)
 #define REG_PMC_FSM				0x0004
+#define REG_SYS_CLKR				0x0008
+#define   SYS_CLKSEL_80M			BIT(0)
+#define   SYS_CPU_CLKSEL			BIT(2)
+#define   SYS_MAC_CLK_EN			BIT(11)
+#define   SYS_SYS_CLK_EN			BIT(12)
+#define   SWHW_SEL				BIT(14)
+#define   FWHW_SEL				BIT(15)
+#define REG_EPROM_CMD				0x000A
+#define   EPROM_SEL				BIT(4)
+#define   EPROM_EN				BIT(5)
+#define REG_AFE_MISC				0x0010
+#define   AFE_MISC_BGEN				BIT(0)
+#define   AFE_MISC_MBEN				BIT(1)
+#define   AFE_MISC_I32_EN			BIT(3)
+#define REG_SPS0_CTRL				0x0011
+#define REG_SPS1_CTRL				0x0018
+#define REG_LDOA15_CTRL				0x0020
+#define   LDA15_EN				BIT(0)
+#define REG_LDOV12D_CTRL			0x0021
+#define   LDV12_EN				BIT(0)
+#define REG_AFE_XTAL_CTRL			0x0026
+#define   XTAL_GATE_AFE				BIT(10)
+#define REG_AFE_PLL_CTRL			0x0028
+#define   APLL_EN				BIT(0)
+#define REG_EFUSE_CTRL				0x0030
+#define REG_EFUSE_TEST				0x0034
+
+/* Command Control Registers */
+#define REG_CR					0x0040
+#define   HCI_TXDMA_EN				BIT(2)
+#define   HCI_RXDMA_EN				BIT(3)
+#define   TXDMA_EN				BIT(4)
+#define   RXDMA_EN				BIT(5)
+#define   FW2HW_EN				BIT(6)
+#define   DDMA_EN				BIT(7)
+#define   MACTXEN				BIT(8)
+#define   MACRXEN				BIT(9)
+#define   SCHEDULE_EN				BIT(10)
+#define   BB_GLB_RSTn				BIT(12)
+#define   BBRSTn				BIT(13)
+#define REG_TCR					0x0044
+#define   IMEM_CHK_RPT				BIT(1)
+#define   EMEM_CHK_RPT				BIT(3)
+#define REG_RCR					0x0048
+
+#define TXDMA_INIT_VALUE			(IMEM_CHK_RPT | EMEM_CHK_RPT)
+
+/* MACID Setting Registers */
+#define REG_MACIDR0				0x0050
+#define REG_MACIDR4				0x0054
+
+/* USB Configuration Registers */
+#define REG_USB_AGG_TO				0xFE5C
+#define REG_USB_MAC_ADDR			0xFE70
 
 #endif
