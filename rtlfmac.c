7439dbdd71112794c88e61b89cefd2f7a6f92d43
Joshua Roys <roysjosh@gmail.com>
2013-04-06 22:43:18 -0400

    Upload firmware and start USB RX

--- rtlfmac.c
+++ rtlfmac.c
@@ -28,7 +28,7 @@
 
 #include "rtlfmac.h"
 
-/* usb communication functions */
+/* USB communication functions */
 static int _usbctrl_vendorreq_sync_read(struct usb_device *udev, u8 request,
 					u16 value, u16 index, void *pdata,
 					u16 len)
@@ -119,6 +119,40 @@ static int _usb_write_sync(struct usb_device *udev, u32 addr, u32 val, u16 len)
 	return ret;
 }
 
+static void _usb_write_bulk_complete(struct urb *_urb)
+{
+	struct sk_buff *skb = (struct sk_buff *)_urb->context;
+	dev_kfree_skb_irq(skb);
+}
+
+static int _usb_write_bulk(struct usb_device *udev, struct sk_buff *skb, u32 ep_num)
+{
+	int ret;
+	struct urb *_urb;
+
+	_urb = usb_alloc_urb(0, GFP_ATOMIC);
+	if (!_urb) {
+		pr_err("%s: failed to allocate URB\n", __func__);
+		dev_kfree_skb(skb);
+		return -ENOMEM;
+	}
+
+	usb_fill_bulk_urb(_urb, udev, usb_sndbulkpipe(udev, ep_num), skb->data, skb->len,
+			_usb_write_bulk_complete, skb);
+	_urb->transfer_flags |= URB_ZERO_PACKET;
+
+	//usb_anchor_urb(_urb, ...); FIXME
+
+	ret = usb_submit_urb(_urb, GFP_ATOMIC);
+	if (ret < 0) {
+		//usb_unanchor_urb(_urb);
+		dev_kfree_skb(skb);
+	}
+	usb_free_urb(_urb);
+
+	return ret;
+}
+
 static u8 rtlfmac_read_byte(struct rtlfmac_cfg80211_priv *priv, u32 addr)
 {
 	return _usb_read_sync(priv->usbdev, addr, 1);
@@ -151,6 +185,190 @@ static int rtlfmac_write_dword(struct rtlfmac_cfg80211_priv *priv, u32 addr, u32
 	return _usb_write_sync(priv->usbdev, addr, val, 4);
 }
 
+static int rtlfmac_write_skb(struct rtlfmac_cfg80211_priv *priv, struct sk_buff *skb)
+{
+	u32 ep_num = priv->ep_mapping[skb_get_queue_mapping(skb)];
+	return _usb_write_bulk(priv->usbdev, skb, ep_num);
+}
+
+/* USB RX functions */
+static void rtlfmac_rx_cleanup(struct rtlfmac_cfg80211_priv *priv)
+{
+	struct urb *urb;
+
+	usb_kill_anchored_urbs(&priv->rx_submitted);
+	while ((urb = usb_get_from_anchor(&priv->rx_submitted))) {
+		usb_free_coherent(priv->usbdev, urb->transfer_buffer_length,
+				urb->transfer_buffer, urb->transfer_dma);
+		usb_free_urb(urb);
+	}
+
+	while ((urb = usb_get_from_anchor(&priv->rx_cleanup))) {
+		usb_free_coherent(priv->usbdev, urb->transfer_buffer_length,
+				urb->transfer_buffer, urb->transfer_dma);
+		usb_free_urb(urb);
+	}
+}
+
+static void rtlfmac_rx_process(struct rtlfmac_cfg80211_priv *priv, struct sk_buff *skb)
+{
+	struct rtlfmac_rx_desc *pdesc;
+
+	pdesc = (struct rtlfmac_rx_desc *)skb->data;
+	skb_pull(skb, RTL_RX_HEADER_SIZE);
+
+	if (pdesc->macid == 0x1f && pdesc->tid == 0x0f) {
+		/* C2H event */
+		u8 evnum;
+		u16 evlen;
+		struct rtlfmac_rx_c2h_desc *c2h;
+
+		c2h = (struct rtlfmac_rx_c2h_desc *)skb->data;
+		skb_pull(skb, 8); /* 8 byte alignment */
+
+		evnum = c2h->evnum;
+		evlen = le16_to_cpu(c2h->len);
+
+		switch(evnum) {
+		case C2H_FWDBG_EVENT:
+			pr_info("%s: fwdbg: %s%s", __func__, skb->data,
+					(skb->data[evlen - 2] == '\n' ? "" : "\n"));
+			break;
+		default:
+			pr_info("%s: unhandled C2H %i\n", __func__, evnum);
+			break;
+		}
+
+		dev_kfree_skb_any(skb);
+
+		return;
+	}
+
+	dev_kfree_skb_any(skb); /* FIXME */
+}
+
+static void rtlfmac_rx_complete(struct urb *urb)
+{
+	int err;
+	struct rtlfmac_cfg80211_priv *priv = (struct rtlfmac_cfg80211_priv *)urb->context;
+
+	if (likely(0 == urb->status)) {
+		struct sk_buff *skb;
+		unsigned int size = urb->actual_length;
+
+		/* FIXME check size */
+
+		skb = dev_alloc_skb(size + 32); /* for radiotap */
+		if (!skb) {
+			pr_err("%s: failed to allocate skb\n", __func__);
+			goto resubmit;
+		}
+		skb_reserve(skb, 32); /* for radiotap */
+		memcpy(skb_put(skb, size), urb->transfer_buffer, size);
+
+		rtlfmac_rx_process(priv, skb);
+
+		goto resubmit;
+	}
+
+	switch(urb->status) {
+	/* disconnect */
+	case -ENOENT:
+	case -ECONNRESET:
+	case -ENODEV:
+	case -ESHUTDOWN:
+		goto free;
+	default:
+		break;
+	}
+
+resubmit:
+	usb_anchor_urb(urb, &priv->rx_submitted);
+	err = usb_submit_urb(urb, GFP_ATOMIC);
+	if (unlikely(err)) {
+		pr_err("%s: failed to submit URB\n", __func__);
+		usb_unanchor_urb(urb);
+		goto free;
+	}
+
+	return;
+
+free:
+	/* On some architectures, usb_free_coherent must not be called from
+	 * hardirq context. Queue urb to cleanup list.
+	 */
+	usb_anchor_urb(urb, &priv->rx_cleanup);
+}
+
+static int rtlfmac_rx_start(struct rtlfmac_cfg80211_priv *priv)
+{
+	int i, ret = 0;
+	struct urb *urb;
+	void *buf;
+
+	init_usb_anchor(&priv->rx_cleanup);
+	init_usb_anchor(&priv->rx_submitted);
+
+	for(i = 0; i < RTL_NUM_RX_URBS; i++) {
+		urb = usb_alloc_urb(0, GFP_KERNEL);
+		if (!urb) {
+			pr_err("%s: failed to allocate URB\n", __func__);
+			ret = -ENOMEM;
+			break;
+		}
+
+		buf = usb_alloc_coherent(priv->usbdev, RTL_MAX_RX_SIZE, GFP_KERNEL,
+				&urb->transfer_dma);
+		if (!buf) {
+			pr_err("%s: failed to allocate USB coherent buffer\n", __func__);
+			usb_free_urb(urb);
+			ret = -ENOMEM;
+			break;
+		}
+
+		usb_fill_bulk_urb(urb, priv->usbdev, usb_rcvbulkpipe(priv->usbdev, priv->in_ep_num),
+				buf, RTL_MAX_RX_SIZE, rtlfmac_rx_complete, priv);
+		urb->transfer_flags |= URB_NO_TRANSFER_DMA_MAP;
+
+		usb_anchor_urb(urb, &priv->rx_submitted);
+		ret = usb_submit_urb(urb, GFP_KERNEL);
+		if (unlikely(ret)) {
+			pr_err("%s: failed to submit URB\n", __func__);
+			usb_unanchor_urb(urb);
+			usb_free_coherent(priv->usbdev, RTL_MAX_RX_SIZE, buf,
+					urb->transfer_dma);
+			usb_free_urb(urb);
+			break;
+		}
+		usb_free_urb(urb);
+	}
+
+	if (unlikely(ret)) {
+		rtlfmac_rx_cleanup(priv);
+	}
+
+	return ret;
+}
+
+#if 0
+static int rtlfmac_write_mem(struct rtlfmac_cfg80211_priv *priv, u8 *data, u32 len, enum rtlfmac_txq qnum)
+{
+	u8 *ptr;
+	u32 ep_num;
+	struct sk_buff *skb;
+
+	skb = dev_alloc_skb(len);
+	if (!skb) {
+		return -ENOMEM;
+	}
+	ptr = skb_put(skb, len);
+	memcpy(ptr, data, len);
+
+	ep_num = priv->ep_mapping[qnum];
+	return _usb_write_bulk(priv->usbdev, skb, ep_num);
+}
+#endif
+
 /* rtlfmac functions */
 int rtlfmac_fw_cmd(struct rtlfmac_cfg80211_priv *priv, uint8_t code, void *buf, int len)
 {
@@ -158,8 +376,9 @@ int rtlfmac_fw_cmd(struct rtlfmac_cfg80211_priv *priv, uint8_t code, void *buf, 
 	struct sk_buff *skb;
 
 	skb = dev_alloc_skb(len);
-	if (!skb)
+	if (!skb) {
 		return -ENOMEM;
+	}
 #endif
 
 	return 0;
@@ -385,8 +604,178 @@ int rtlfmac_chip_init(struct rtlfmac_cfg80211_priv *priv)
 	return 0;
 }
 
+static int rtlfmac_chip_init_complete(struct rtlfmac_cfg80211_priv *priv)
+{
+	u8 val8;
+	u32 val32;
+
+	/* Append PHY status, Enable RX TCP checksum offload */
+	val32 = rtlfmac_read_dword(priv, REG_RCR);
+	rtlfmac_write_dword(priv, REG_RCR, val32 | APP_PHYST_RXFF | RX_TCPOFDL_EN);
+	pr_info("%s: RCR=0x%08x\n", __func__, rtlfmac_read_dword(priv, REG_RCR));
+	/* Set MAC lookback to normal mode */
+	rtlfmac_write_byte(priv, REG_LBKMD_SEL, LBK_NORMAL);
+	/* Set TX/RX page size */
+	val8 = rtlfmac_read_byte(priv, REG_PBP);
+	rtlfmac_write_byte(priv, REG_PBP, val8 | PBP_PAGE_128B);
+	/* Enable USB RX aggregation */
+	val8 = rtlfmac_read_byte(priv, REG_RXDMA_RXCTRL);
+	rtlfmac_write_byte(priv, REG_RXDMA_RXCTRL, val8 | RXDMA_AGG_EN);
+	/* Set USB aggregation threshold (1 means no USB aggregation) */
+	rtlfmac_write_byte(priv, REG_RXDMA_AGG_PG_TH, 1);
+	/* Set USB aggregation timeout to 1.7ms/4 */
+	rtlfmac_write_byte(priv, REG_USB_DMA_AGG_TO, 0x04);
+	/* Fix USB RX FIFO error - done in chip init */
+	/* Set MAC address */
+	for(val8 = 0; val8 < 6; val8++) {
+		rtlfmac_write_byte(priv, REG_MACIDR0, priv->wiphy->perm_addr[val8]);
+	}
+
+	return 0;
+}
+
 /* firmware loading functions */
 static const char firmware_file[] = "rtlwifi/rtl8712u.bin";
+
+static void rtlfmac_upload_fw(struct rtlfmac_cfg80211_priv *priv, const struct firmware *firmware)
+{
+	int i, state;
+	struct rtlfmac_fw_hdr *fw_hdr;
+	struct rtlfmac_fw_priv *fw_priv;
+	struct rtlfmac_tx_desc *pdesc;
+	struct sk_buff *skb;
+	u8 *imem, *emem, *data, *ptr;
+	u16 sig, val16, mask16;
+	u32 fw_size, dmem_size, img_imem_size, img_sram_size, fw_priv_size, len, frag_len;
+
+	fw_hdr = (struct rtlfmac_fw_hdr *)firmware->data;
+	fw_size = firmware->size;
+
+	sig = le16_to_cpu(fw_hdr->signature);
+	if (sig != 0x8712 && sig != 0x8192) {
+		pr_err("%s: invalid firmware signature\n", __func__);
+		return;
+	}
+	dmem_size = le32_to_cpu(fw_hdr->dmem_size);
+	img_imem_size = le32_to_cpu(fw_hdr->img_imem_size);
+	img_sram_size = le32_to_cpu(fw_hdr->img_sram_size);
+	fw_priv_size = le32_to_cpu(fw_hdr->fw_priv_size);
+
+#if 0
+	if (dmem_size + img_imem_size + img_sram_size + fw_priv_size > fw_size) {
+		pr_err("%s: \n");
+	}
+#endif
+
+	/* setup fw_priv */
+	fw_priv = (struct rtlfmac_fw_priv *)(firmware->data + sizeof(struct rtlfmac_fw_hdr));
+	memset(fw_priv, 0, sizeof(struct rtlfmac_fw_priv));
+
+	fw_priv->hci_sel = 0x12;
+	fw_priv->rf_config = 0x11;
+	fw_priv->usb_ep_num = priv->num_endpoints;
+#if 0
+	fw_priv->turbo_mode = 1;
+	fw_priv->low_power_mode = 0;
+	fw_priv->mp_mode = 0;
+	fw_priv->bw_40mhz_en = 0;
+	fw_priv->vcs_type = 2;
+	fw_priv->vcs_mode = 1;
+#endif
+
+	imem = (u8 *)firmware->data + sizeof(struct rtlfmac_fw_hdr) + sizeof(struct rtlfmac_fw_priv);
+	emem = imem + img_imem_size;
+
+	for(state = 0; state < 5; state++) {
+		switch(state) {
+		case 0: /* IMEM */
+			data = imem;
+			len = img_imem_size;
+			break;
+		case 1: /* EMEM */
+			data = emem;
+			len = img_sram_size;
+			break;
+		case 2: /* Enable CPU */
+			val16 = rtlfmac_read_word(priv, REG_SYS_CLKR);
+			rtlfmac_write_word(priv, REG_SYS_CLKR, val16 | SYS_CPU_CLKSEL);
+			val16 = rtlfmac_read_word(priv, REG_SYS_FUNC_EN);
+			rtlfmac_write_word(priv, REG_SYS_FUNC_EN, val16 | FEN_CPUEN);
+			len = 0;
+			break;
+		case 3: /* DMEM */
+			data = (u8 *)fw_priv;
+			len = sizeof(struct rtlfmac_fw_priv);
+			break;
+		case 4: /* Final check */
+			len = 0;
+			break;
+		}
+
+		while (len > 0) {
+			if (len > 0xC000) {
+				frag_len = 0xC000;
+			} else {
+				frag_len = len;
+			}
+
+			skb = dev_alloc_skb(frag_len);
+			if (!skb) {
+				pr_err("%s: failed to allocate skb\n", __func__);
+				return;
+			}
+			ptr = skb_put(skb, frag_len);
+			memcpy(ptr, data, frag_len);
+			pdesc = (struct rtlfmac_tx_desc *)skb_push(skb, RTL_TX_HEADER_SIZE);
+			memset(pdesc, 0, RTL_TX_HEADER_SIZE);
+			pdesc->pkt_size = cpu_to_le32(frag_len);
+			pdesc->linip = (frag_len == len ? 1 : 0);
+
+			skb->queue_mapping = RTL_TXQ_VO;
+			rtlfmac_write_skb(priv, skb);
+
+			len -= frag_len;
+			data += frag_len;
+		}
+
+		switch(state) {
+		case 0:
+			mask16 = IMEM_CODE_DONE | IMEM_CHK_RPT;
+			i = 10;
+			break;
+		case 1:
+			mask16 = EMEM_CODE_DONE | EMEM_CHK_RPT;
+			i = 5;
+			break;
+		case 2:
+			mask16 = IMEM_RDY;
+			i = 200;
+			break;
+		case 3:
+			mask16 = DMEM_CODE_DONE;
+			i = 200;
+			break;
+		case 4:
+			mask16 = FWRDY;
+			i = 600;
+			break;
+		}
+
+		do {
+			val16 = rtlfmac_read_word(priv, REG_TCR);
+			if (val16 & mask16)
+				break;
+			udelay(10);
+			i--;
+		} while (i > 0);
+		if (i == 0) {
+			pr_err("%s: firmware upload failed\n", __func__);
+			return;
+		}
+	}
+
+	pr_info("%s: firmware upload complete\n", __func__);
+}
 
 static void rtlfmac_fw_cb(const struct firmware *firmware, void *context)
 {
@@ -398,9 +787,12 @@ static void rtlfmac_fw_cb(const struct firmware *firmware, void *context)
 		return;
 	}
 
-	priv->fw = firmware;
-
 	rtlfmac_chip_init(priv);
+	rtlfmac_upload_fw(priv, firmware);
+	release_firmware(firmware);
+	rtlfmac_chip_init_complete(priv);
+
+	rtlfmac_rx_start(priv);
 }
 
 int rtlfmac_load_fw(struct rtlfmac_cfg80211_priv *priv)
@@ -408,7 +800,6 @@ int rtlfmac_load_fw(struct rtlfmac_cfg80211_priv *priv)
 	int rc;
 
 	init_completion(&priv->fw_ready);
-	priv->fw = NULL;
 
 	pr_info("%s: loading firmware from \"%s\"\n", __func__, firmware_file);
 	rc = request_firmware_nowait(THIS_MODULE, 1, firmware_file, priv->dev,
@@ -439,7 +830,6 @@ static struct rtlfmac_cfg80211_priv *rtlfmac_alloc_wiphy(void)
 	priv->wiphy = wiphy;
 
 	/* fill out wiphy */
-	//wiphy->perm_addr
 	//wiphy->privid
 	wiphy->interface_modes = BIT(NL80211_IFTYPE_STATION);
 	wiphy->max_scan_ssids = 1;
@@ -486,9 +876,47 @@ int rtlfmac_probe(struct usb_interface *intf,
 	priv->usbdev = usb;
 	priv->dev = &usb->dev;
 
+	/* usb endpoint mapping */
+	priv->num_endpoints = intf->cur_altsetting->desc.bNumEndpoints;
+
+	priv->in_ep_num = 0x3;
+	priv->ep_mapping[RTL_TXQ_BE] = 0x6;
+	priv->ep_mapping[RTL_TXQ_VO] = 0x4;
+	priv->ep_mapping[RTL_TXQ_H2CCMD] = 0xd;
+	switch(priv->num_endpoints) {
+	case 4:
+		priv->ep_mapping[RTL_TXQ_BK] = 0x6;
+		priv->ep_mapping[RTL_TXQ_VI] = 0x4;
+		priv->ep_mapping[RTL_TXQ_BCN] = 0xd;
+		priv->ep_mapping[RTL_TXQ_HI] = 0xd;
+		priv->ep_mapping[RTL_TXQ_MGT] = 0xd;
+		break;
+	case 6:
+		priv->ep_mapping[RTL_TXQ_BK] = 0x7;
+		priv->ep_mapping[RTL_TXQ_VI] = 0x5;
+		priv->ep_mapping[RTL_TXQ_BCN] = 0xd;
+		priv->ep_mapping[RTL_TXQ_HI] = 0xd;
+		priv->ep_mapping[RTL_TXQ_MGT] = 0xd;
+		break;
+	case 11:
+		priv->ep_mapping[RTL_TXQ_BK] = 0x7;
+		priv->ep_mapping[RTL_TXQ_VI] = 0x5;
+		priv->ep_mapping[RTL_TXQ_BCN] = 0xa;
+		priv->ep_mapping[RTL_TXQ_HI] = 0xb;
+		priv->ep_mapping[RTL_TXQ_MGT] = 0xc;
+		break;
+	default:
+		pr_err("%s: unknown number of endpoints!\n", __func__);
+		wiphy_free(priv->wiphy);
+		return -1;
+	}
+
 	/* driver state */
 	dev_set_drvdata(priv->dev, priv);
 	set_wiphy_dev(priv->wiphy, priv->dev);
+
+	/* ensure chip is in initial state */
+	rtlfmac_write_byte(priv, REG_USB_HRPWM, 0x00);
 
 	/* read chip version */
 	tmpb = (rtlfmac_read_dword(priv, REG_PMC_FSM) >> 15) & 0x1f;
@@ -537,8 +965,8 @@ static void rtlfmac_disconnect(struct usb_interface *intf)
 	priv = dev_get_drvdata(&usb->dev);
 
 	wait_for_completion(&priv->fw_ready);
-	if (priv->fw)
-		release_firmware(priv->fw);
+
+	rtlfmac_rx_cleanup(priv);
 #if 0
 	wiphy_unregister(priv->wiphy);
 #endif
--- rtlfmac.h
+++ rtlfmac.h
@@ -33,60 +33,295 @@
 #define   REALTEK_USB_VENQT_CMD_IDX	0x00
 #define MAX_USBCTRL_VENDORREQ_TIMES	10
 
+#define RTL_NUM_RX_URBS			8
+#define RTL_MAX_RX_SIZE			9100
+
+#define RTL_RX_HEADER_SIZE		24
+#define RTL_TX_HEADER_SIZE		32
+
+enum rtlfmac_txq {
+#if 0
+	RTL_TXQ_BK,
+	RTL_TXQ_BE,
+	RTL_TXQ_VI,
+	RTL_TXQ_VO,
+#endif
+	RTL_TXQ_VO,
+	RTL_TXQ_VI,
+	RTL_TXQ_BE,
+	RTL_TXQ_BK,
+
+	RTL_TXQ_BCN,
+	RTL_TXQ_HI,
+	RTL_TXQ_MGT,
+	RTL_TXQ_H2CCMD,
+
+	__RTL_TXQ_NUM,
+};
+
+struct rtlfmac_rx_desc {
+	/* dword0 */
+	u16 pkt_len:14;
+	u8 crc32:1;
+	u8 icv:1;
+	u8 drvinfo_size:4;
+	u8 security:3;
+	u8 qos:1;
+	u8 shift:2;
+	u8 phy_status:1;
+	u8 swdec:1;
+	u8 last_seg:1;
+	u8 first_seg:1;
+	u8 eor:1;
+	u8 own:1;
+
+	/* dword1 */
+	u8 macid:5;
+	u8 tid:4;
+	u8 reserved01_09:5;
+	u8 paggr:1;
+	u8 faggr:1;
+	u8 a1_fit:4;
+	u8 a2_fit:4;
+	u8 pam:1;
+	u8 pwr:1;
+	u8 more_data:1;
+	u8 more_frag:1;
+	u8 type:2;
+	u8 mc:1;
+	u8 bc:1;
+
+	/* dword2 */
+	u16 seq:12;
+	u8 frag:4;
+	u8 next_pktlen;
+	u8 reserved02_24:6;
+	u8 next_ind:1;
+	u8 reserved02_31:1;
+
+	/* dword3 */
+	u8 rx_mcs:6;
+	u8 rx_ht:1;
+	u8 amsdu:1;
+	u8 splcp:1;
+	u8 bw:1;
+	u8 htc:1;
+	u8 tcp_chk_rpt:1;
+	u8 ip_chk_rpt:1;
+	u8 tcp_chk_valid:1;
+	u8 hwpc_err:1;
+	u8 hwpc_ind:1;
+	u16 iv0;
+
+	/* dword4 */
+	u32 iv1;
+
+	/* dword5 */
+	u32 tsfl;
+} __packed;
+
+struct rtlfmac_rx_c2h_desc {
+	/* dword0 */
+	u16 len;
+	u8 evnum;
+	u8 seqno:7;
+	u8 frag:1;
+} __packed;
+
+struct rtlfmac_tx_desc {
+	/* dword0 */
+	u16 pkt_size;
+	u8 offset;
+	u8 type:2;
+	u8 last_seg:1;
+	u8 first_seg:1;
+	u8 linip:1;
+	u8 amsdu:1;
+	u8 reserved00_30:1;
+	u8 own:1;
+
+	/* dword1 */
+	u8 macid:5;
+	u8 more_data:1;
+	u8 more_frag:1;
+	u8 pifs:1;
+	u8 queue_sel:5;
+	u8 ack_policy:2;
+	u8 no_acm:1;
+	u8 non_qos:1;
+	u8 key_id:2;
+	u8 oui:1;
+	u8 pkt_type:1;
+	u8 en_desc_id:1;
+	u8 sec_type:2;
+	u8 wds:1;
+	u8 htc:1;
+	u8 pkt_offset:5;
+	u8 hwpc:1;
+
+	/* dword2 */
+	u8 data_retry_limit:6;
+	u8 retry_limit_enable:1;
+	u8 bmc:1;
+	u8 reserved02_08:4;
+	u8 rts_retry_count:6;
+	u8 data_retry_count:6;
+	u8 rsvd_macid:5;
+	u8 agg_enable:1;
+	u8 agg_break:1;
+	u8 own_mac:1;
+
+	/* dword3 */
+	u8 next_heap_page;
+	u8 tail_page;
+	u16 seq:12;
+	u8 frag:4;
+
+	/* dword4 */
+	u8 rts_rate:6;
+	u8 disable_rts_fb:1;
+	u8 rts_rate_fb_limit:4;
+	u8 cts_enable:1;
+	u8 rts_enable:1;
+	u8 ra_brsr_id:3;
+	u8 tx_ht:1;
+	u8 tx_short:1;
+	u8 tx_bandwidth:1;
+	u8 tx_subcarrier:2;
+	u8 tx_stbc:2;
+	u8 tx_reverse_direction:1;
+	u8 rts_ht:1;
+	u8 rts_short:1;
+	u8 rts_bandwidth:1;
+	u8 rts_subcarrier:2;
+	u8 rts_stbc:2;
+	u8 user_rate:1;
+
+	/* dword5 */
+	u16 packet_id:9;
+	u8 tx_rate:6;
+	u8 disable_fb:1;
+	u8 data_rate_fb_limit:5;
+	u16 tx_agc:11;
+
+	/* dword6 */
+	u16 ip_check_sum;
+	u16 tcp_check_sum;
+
+	/* dword7 */
+	u16 tx_buffer_size;
+	u8 cmd_seq;
+	u8 reserved07_24;
+} __packed;
+
+enum fw_c2h_event {
+	C2H_READ_MACREG_EVENT,				/* 0 */
+	C2H_READBB_EVENT,
+	C2H_READRF_EVENT,
+	C2H_READ_EEPROM_EVENT,
+	C2H_READ_EFUSE_EVENT,
+	C2H_READ_CAM_EVENT,				/* 5 */
+	C2H_GET_BASIC_RATE_EVENT,
+	C2H_GET_DATA_RATE_EVENT,
+	C2H_SURVEY_EVENT,
+	C2H_SURVEY_DONE_EVENT,
+	C2H_JOIN_BSS_EVENT,				/* 10 */
+	C2H_ADD_STA_EVENT,
+	C2H_DEL_STA_EVENT,
+	C2H_ATIM_DONE_EVENT,
+	C2H_TX_REPORT_EVENT,
+	C2H_CCX_REPORT_EVENT,				/* 15 */
+	C2H_DTM_REPORT_EVENT,
+	C2H_TX_RATE_STATS_EVENT,
+	C2H_C2H_LBK_EVENT,
+	C2H_FWDBG_EVENT,
+	C2H_C2HFEEDBACK_EVENT,				/* 20 */
+	C2H_ADDBA_EVENT,
+	C2H_HBCN_EVENT,
+	C2H_REPORT_PWR_STATE_EVENT,
+	C2H_WPS_PBC_EVENT,
+	C2H_ADDBA_REPORT_EVENT,				/* 25 */
+};
+
 enum fw_h2c_cmd {
-	H2C_READ_MACREG_CMD,				/*0*/
+	H2C_READ_MACREG_CMD,				/* 0 */
 	H2C_WRITE_MACREG_CMD,
 	H2C_READBB_CMD,
 	H2C_WRITEBB_CMD,
 	H2C_READRF_CMD,
-	H2C_WRITERF_CMD,				/*5*/
+	H2C_WRITERF_CMD,				/* 5 */
 	H2C_READ_EEPROM_CMD,
 	H2C_WRITE_EEPROM_CMD,
 	H2C_READ_EFUSE_CMD,
 	H2C_WRITE_EFUSE_CMD,
-	H2C_READ_CAM_CMD,				/*10*/
+	H2C_READ_CAM_CMD,				/* 10 */
 	H2C_WRITE_CAM_CMD,
 	H2C_SETBCNITV_CMD,
 	H2C_SETMBIDCFG_CMD,
 	H2C_JOINBSS_CMD,
-	H2C_DISCONNECT_CMD,				/*15*/
+	H2C_DISCONNECT_CMD,				/* 15 */
 	H2C_CREATEBSS_CMD,
-	H2C_SETOPMode_CMD,
+	H2C_SETOPMODE_CMD,
 	H2C_SITESURVEY_CMD,
 	H2C_SETAUTH_CMD,
-	H2C_SETKEY_CMD,					/*20*/
+	H2C_SETKEY_CMD,					/* 20 */
 	H2C_SETSTAKEY_CMD,
 	H2C_SETASSOCSTA_CMD,
 	H2C_DELASSOCSTA_CMD,
 	H2C_SETSTAPWRSTATE_CMD,
-	H2C_SETBASICRATE_CMD,				/*25*/
+	H2C_SETBASICRATE_CMD,				/* 25 */
 	H2C_GETBASICRATE_CMD,
 	H2C_SETDATARATE_CMD,
 	H2C_GETDATARATE_CMD,
 	H2C_SETPHYINFO_CMD,
-	H2C_GETPHYINFO_CMD,				/*30*/
+	H2C_GETPHYINFO_CMD,				/* 30 */
 	H2C_SETPHY_CMD,
 	H2C_GETPHY_CMD,
 	H2C_READRSSI_CMD,
 	H2C_READGAIN_CMD,
-	H2C_SETATIM_CMD,				/*35*/
+	H2C_SETATIM_CMD,				/* 35 */
 	H2C_SETPWRMODE_CMD,
 	H2C_JOINBSSRPT_CMD,
 	H2C_SETRATABLE_CMD,
 	H2C_GETRATABLE_CMD,
-	H2C_GETCCXREPORT_CMD,				/*40*/
+	H2C_GETCCXREPORT_CMD,				/* 40 */
 	H2C_GETDTMREPORT_CMD,
 	H2C_GETTXRATESTATICS_CMD,
 	H2C_SETUSBSUSPEND_CMD,
 	H2C_SETH2CLBK_CMD,
-	H2C_TMP1,					/*45*/
-	H2C_WOWLAN_UPDATE_GTK_CMD,
-	H2C_WOWLAN_FW_OFFLOAD,
-	H2C_TMP2,
-	H2C_TMP3,
-	H2C_WOWLAN_UPDATE_IV_CMD,			/*50*/
-	H2C_TMP4,
-	MAX_H2CCMD					/*52*/
+	H2C_ADDBA_REQ_CMD,				/* 45 */
+	H2C_SETCHANNEL_CMD,
+	H2C_SET_TXPOWER_CMD,
+	H2C_SWITCH_ANTENNA_CMD,
+	H2C_SET_XTAL_CAP_CMD,
+	H2C_SET_SINGLE_CARRIER_TX_CMD,			/* 50 */
+	H2C_SET_SINGLE_TONE_CMD,
+	H2C_SET_CARRIER_SUPPRESION_TX_CMD,
+	H2C_SET_CONTINOUS_TX_CMD,
+	H2C_SWITCH_BW_CMD,
+	H2C_TX_BEACON_CMD,				/* 55 */
+	H2C_SET_POWER_TRACKING_CMD,
+	H2C_AMSDU_TO_AMPDU_CMD,
+	H2C_SET_MAC_ADDRESS_CMD,
+	H2C_DISCONNECT_CTRL_CMD,
+	H2C_SET_CHANNELPLAN_CMD,			/* 60 */
+	H2C_DISCONNECT_CTRL_EX_CMD,
+	H2C_GET_H2C_LBK_CMD,
+	H2C_SET_PROBE_REQ_EXTRA_IE_CMD,
+	H2C_SET_ASSOC_REQ_EXTRA_IE_CMD,
+	H2C_SET_PROBE_RSP_EXTRA_IE_CMD,			/* 65 */
+	H2C_SET_ASSOC_RSP_EXTRA_IE_CMD,
+	H2C_GET_CURRENT_DATA_RATE_CMD,
+	H2C_GET_TX_RETRY_CNT_CMD,
+	H2C_GET_RX_RETRY_CNT_CMD,
+	H2C_GET_BCN_OK_CNT_CMD,				/* 70 */
+	H2C_GET_BCN_ERR_CNT_CMD,
+	H2C_GET_CURRENT_TXPOWER_CMD,
+	H2C_SET_DIG_CMD,
+	H2C_SET_RA_CMD,
+	H2C_SET_PT_CMD,					/* 75 */
+	H2C_READ_RSSI_CMD,
+	MAX_H2CCMD,					/* 77 */
 };
 
 struct rtlfmac_sitesurvey_cmd {
@@ -94,7 +329,70 @@ struct rtlfmac_sitesurvey_cmd {
 	u32 bsslimit;
 	u32 ssidlen;
 	u8 ssid[IEEE80211_MAX_SSID_LEN + 1];
-};
+} __packed;
+
+struct rtlfmac_fw_hdr {
+	u16 signature;
+	u16 version;
+
+	u32 dmem_size;
+	u32 img_imem_size;
+	u32 img_sram_size;
+	u32 fw_priv_size;
+
+	u32 reserved20;
+	u32 reserved24;
+	u32 reserved28;
+} __packed;
+
+struct rtlfmac_fw_priv {
+	u16 signature;
+	u8 hci_sel;
+	u8 chip_version;
+	u16 customer_id;
+	u8 rf_config;
+	u8 usb_ep_num;
+
+	u32 regulatory_class;
+	u8 rfintfs;
+	u8 def_nettype;
+	u8 turbo_mode;
+	u8 low_power_mode;
+
+	u8 lbk_mode;
+	u8 mp_mode;
+	u8 vcs_type;
+	u8 vcs_mode;
+	u8 reserved20;
+	u8 reserved21;
+	u8 reserved22;
+	u8 reserved23;
+
+	u8 qos_en;
+	u8 bw_40mhz_en;
+	u8 amsdu2ampdu_en;
+	u8 ampdu_en;
+	u8 rate_control_offload;
+	u8 aggregation_offload;
+	u8 reserved30;
+	u8 reserved31;
+
+	u8 beacon_offload;
+	u8 mlme_offload;
+	u8 hwpc_offload;
+	u8 tcp_checksum_offload;
+	u8 tcp_offload;
+	u8 ps_control_offload;
+	u8 wwlan_offload;
+	u8 reserved39;
+
+	u16 tcp_tx_frame_len;
+	u16 tcp_rx_frame_len;
+	u8 reserved44;
+	u8 reserved45;
+	u8 reserved46;
+	u8 reserved47;
+} __packed;
 
 struct rtlfmac_cfg80211_priv {
 	struct usb_device *usbdev;
@@ -102,10 +400,15 @@ struct rtlfmac_cfg80211_priv {
 
 	struct wiphy *wiphy;
 
+	u8 num_endpoints;
+	u32 in_ep_num;
+	u32 ep_mapping[__RTL_TXQ_NUM];
 	u8 hwrev;
 
+	struct usb_anchor rx_cleanup;
+	struct usb_anchor rx_submitted;
+
 	struct completion fw_ready;
-	const struct firmware *fw;
 };
 
 static inline struct rtlfmac_cfg80211_priv *wiphy_to_cfg(struct wiphy *w)
@@ -150,6 +453,7 @@ static inline struct rtlfmac_cfg80211_priv *wiphy_to_cfg(struct wiphy *w)
 #define   APLL_EN				BIT(0)
 #define REG_EFUSE_CTRL				0x0030
 #define REG_EFUSE_TEST				0x0034
+#define   LDOE25_EN				BIT(31)
 
 /* Command Control Registers */
 #define REG_CR					0x0040
@@ -164,18 +468,42 @@ static inline struct rtlfmac_cfg80211_priv *wiphy_to_cfg(struct wiphy *w)
 #define   SCHEDULE_EN				BIT(10)
 #define   BB_GLB_RSTn				BIT(12)
 #define   BBRSTn				BIT(13)
+#define REG_TXPAUSE				0x0042
+#define REG_LBKMD_SEL				0x0043
 #define REG_TCR					0x0044
+#define   IMEM_CODE_DONE			BIT(0)
 #define   IMEM_CHK_RPT				BIT(1)
+#define   EMEM_CODE_DONE			BIT(2)
 #define   EMEM_CHK_RPT				BIT(3)
+#define   DMEM_CODE_DONE			BIT(4)
+#define   IMEM_RDY				BIT(5)
+#define   BASECHG				BIT(6)
+#define   FWRDY					BIT(7)
 #define REG_RCR					0x0048
+#define   APP_PHYST_RXFF			BIT(25)
+#define   RX_TCPOFDL_EN				BIT(26)
 
+#define LBK_NORMAL				0x00
 #define TXDMA_INIT_VALUE			(IMEM_CHK_RPT | EMEM_CHK_RPT)
 
 /* MACID Setting Registers */
 #define REG_MACIDR0				0x0050
 #define REG_MACIDR4				0x0054
 
+/* FIFO Control Registers */
+#define REG_PBP					0x00B5
+#define   PBP_PAGE_128B				BIT(0)
+#define   PBP_PAGE_256B				BIT(1)
+#define   PBP_PAGE_512B				BIT(2)
+#define   PBP_PAGE_1024B			BIT(3)
+#define   PBP_PAGE_2048B			BIT(4)
+#define REG_RXDMA_RXCTRL			0x00BD
+#define   RXDMA_AGG_EN				BIT(7)
+#define REG_RXDMA_AGG_PG_TH			0x00D9
+
 /* USB Configuration Registers */
+#define REG_USB_HRPWM				0xFE58
+#define REG_USB_DMA_AGG_TO			0xFE5B
 #define REG_USB_AGG_TO				0xFE5C
 #define REG_USB_MAC_ADDR			0xFE70
 
